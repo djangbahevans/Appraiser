5a40962ee61c5884871f6bf61beb9e83444cc6af
Evans Djangbah <djangbahevans@yahoo.com>
2022-03-17 07:05:58 +0000

    Removed unused utils

--- app/main.py
+++ app/main.py
@@ -4,11 +4,22 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.security import OAuth2PasswordBearer
 
 from app.database import engine
+from app.routers.appraiser import main as appraiser
+from app.routers.auth_router import main as auth
+from app.routers.email import end as end
+from app.routers.email import mid as mid
+from app.routers.email import start as start
+from app.routers.phase1_router import main as phase1
+from app.routers.phase2_router import main as phase2
+from app.routers.phase3_router import main as phase3
+from app.routers.phase4_router import main as phase4
+from app.routers.staff_router import main as staff
+from app.routers.user_router import main as user
+from app.routers.user_router import models
 
 api = FastAPI(docs_url="/api/docs")
 # INITIATE AUTHENTICATION SCHEME
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/user/authenticate")
-
 
 # GIVE PERMISSION TO FRONTEND
 origins = ["*", "http://localhost"]
@@ -20,41 +31,31 @@ api.add_middleware(
     allow_headers=["*"],
 )
 
-# IMPORT MODELS
-from app.routers.auth_router import models
-from app.routers.user_router import models
-
 models.Base.metadata.create_all(bind=engine)
-
-from app.routers.appraiser import main as appraiser
-from app.routers.auth_router import main as auth
-# from app.routers.email import main as email
-from app.routers.email import end as end
-from app.routers.email import mid as mid
-from app.routers.email import start as start
-# IMPORT ALL ROUTERS
-from app.routers.phase1_router import main as phase1
-from app.routers.phase2_router import main as phase2
-from app.routers.phase3_router import main as phase3
-from app.routers.phase4_router import main as phase4
-from app.routers.staff_router import main as staff
-from app.routers.user_router import main as user
 
 # CUSTOMIZE ALL ENDPOINT HEADERS
 api.include_router(auth.router, prefix="/auth", tags=["User Login"])
 api.include_router(user.router, prefix="/user", tags=["User"])
-api.include_router(staff.router,prefix="/api/staff",tags=["Staff"])
-api.include_router(appraiser.router,prefix="/api/appraiser", tags=["Appraiser"])
-api.include_router(phase1.router,prefix="/api/review",tags=["Start Review"])
-api.include_router(phase2.router, prefix="/api/midyearreview", tags=["Mid-Year Review"])
-api.include_router(phase3.router, prefix="/api/endofyearreview", tags=["End of Year Review"])
-api.include_router(phase4.router, prefix="/api/finalremarks", tags=["Final Remarks"])
+api.include_router(staff.router, prefix="/api/staff", tags=["Staff"])
+api.include_router(
+    appraiser.router, prefix="/api/appraiser", tags=["Appraiser"])
+api.include_router(phase1.router, prefix="/api/review", tags=["Start Review"])
+api.include_router(phase2.router, prefix="/api/midyearreview",
+                   tags=["Mid-Year Review"])
+api.include_router(phase3.router, prefix="/api/endofyearreview",
+                   tags=["End of Year Review"])
+api.include_router(phase4.router, prefix="/api/finalremarks",
+                   tags=["Final Remarks"])
 # api.include_router(email.router, prefix="/email", tags=["E-mails"])
-api.include_router(start.router, prefix="/email", tags=["Start of Year Review Emails"])
-api.include_router(mid.router, prefix="/email", tags=["Mid-Year Review Emails"])
+api.include_router(start.router, prefix="/email",
+                   tags=["Start of Year Review Emails"])
+api.include_router(mid.router, prefix="/email",
+                   tags=["Mid-Year Review Emails"])
 api.include_router(end.router, prefix="/email", tags=["End of Year Emails"])
 
 # DEFAULT ENDPOINT
+
+
 @api.get("/")
 def welcome():
     return "BACKEND OF APPRAISAL MANAGEMENT APP (url/api/docs)"
--- app/utils.py
+++ app/utils.py
@@ -1,74 +1,43 @@
 # IMPORT DEENDENCIES
+import random
+import string
 from datetime import datetime, timedelta
-import string, random, jwt, sys
-from app.exceptions import FileReadFailed
 from typing import Optional
+
+import jwt
+
 from app.config import settings
-import pandas as pd
 
 # DEFINE CHARACTERS USED IN TOKEN
 uppercase_and_digits = string.ascii_uppercase + string.digits
 lowercase_and_digits = string.ascii_lowercase + string.digits
 
+
 # GENERATE ACCESS CODES
 def gen_alphanumeric_code(length):
-    code = ''.join((random.choice(uppercase_and_digits) for i in range(length)))
+    code = ''.join((random.choice(uppercase_and_digits)
+                   for i in range(length)))
     return code
 
+
 # CREATE ACCESS TOKEN
-def create_token(data:dict, expires_delta:Optional[timedelta]=None):
+def create_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
     if expires_delta:
-        expire = datetime.utcnow() + expires_delta # DEFINE EXPIRY DATE FOR ACCESS TOKEN
+        # DEFINE EXPIRY DATE FOR ACCESS TOKEN
+        expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=30) # EXPIRES 30 MINUTES AFTER CREATION
+        # EXPIRES 30 MINUTES AFTER CREATION
+        expire = datetime.utcnow() + timedelta(minutes=30)
     to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM) # GET SECRET KEY FOR TOKEN FROM CONFIG.PY
+    # GET SECRET KEY FOR TOKEN FROM CONFIG.PY
+    encoded_jwt = jwt.encode(
+        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
 
+
 # DECRYPT ACCESS TOKEN
-def decode_token(*, data:str):
+def decode_token(*, data: str):
     to_decode = data
-    return jwt.decode(to_decode, settings.SECRET_KEY, algorithm=settings.ALGORITHM) # GET SECRET KEY FOR TOKEN FROM CONFIG.PY
-
-# GET XCL FILE CONTENTS
-async def read_xcl_file_contents(file, header):
-    file = await file.read()
-    try:
-        return pd.read_excel(file, names=header)
-    except ValueError:
-        raise ValueError
-    except:
-        raise FileReadFailed('{}'.format(sys.exc_info()[0]))
-
-# READ CSV FILE CONTENTS
-async def read_csv_file_contents(file, header):
-    file = await file.read()
-    try:
-        return pd.read_excel(file, names=header)
-    except ValueError:
-        raise ValueError
-    except:
-        raise FileReadFailed()
-
-# BOOLEAN LOGIC
-def logical_xor(a, b):
-    return bool(a) ^ bool(b)
-
-# GET PAIR COMBINATIONS FOR ACCESS TOKEN
-def get_list_pairs(list1, list2, index:int=0):
-    combination = []
-    for i1 in list1:
-        for i2 in list2:
-            combination.append((i1,i2, str(gen_2_pow_n(index))))
-            index+=1
-    return combination
-
-# GENERATE POW
-def gen_2_pow_n(pow):
-    return 2**pow
-
-# TIME-STAMP
-def timestamp_to_datetime(timestamp):
-    dt_obj = datetime.fromtimestamp(timestamp)
-    return dt_obj
+    # GET SECRET KEY FOR TOKEN FROM CONFIG.PY
+    return jwt.decode(to_decode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
